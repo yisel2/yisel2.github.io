112c2262c1b8d26e644785b7bd63723549c6c6d7
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-12 10:16:47 -0300

    margenes y contacto

--- js/skills.js
+++ js/skills.js
@@ -15,7 +15,7 @@ $('.hideme-skill').each( function(i){
     if( bottom_of_window > bottom_of_object ){
 
         $(this).removeClass('hidden');
-        $(this).addClass('animated fadeIn');
+        $(this).addClass('animated bounceIn');
     }
 
 }); 
