9951a6e1c5ab53f92d17959df758358eec22d126
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-15 00:33:06 -0300

    menu js

---
+++ js/menu.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    
+    //cuando la altura de un div sea igual o mayor al 70% de la altura del navegador remueve el activo y agrega al nuevo
+    
+    $(window).scroll( function(){
+        var scroll = $(window).scrollTop();
+        
+        var home_start = $("#home").position().top;
+        var home_end = $("#home").position().top + $("#home").outerHeight();
+        
+        var about_start = $("#about-me").position().top;
+        var about_end = $("#about-me").position().top + $("#about-me").outerHeight();
+        
+        var portfolio_start = $("#portfolio").position().top;
+        var portfolio_end = $("#portfolio").position().top + $("#portfolio").outerHeight();
+        
+        var contact_start = $("#contact").position().top;
+        var contact_end = $("#contact").position().top + $("#contact").outerHeight();
+        
+        if( scroll >= (home_start * 0.90) && ( scroll <= (home_end * 0.55) ) ){  
+            $(".menu-icons-div").each( function(i){
+                $(this).removeClass("active");
+            });
+            $("#nav-home").addClass("active");
+        }
+        
+        if( ( scroll >= (about_start * 0.80) ) && ( scroll <= (about_end * 0.70) ) ){  
+            $(".menu-icons-div").each( function(i){
+                $(this).removeClass("active");
+            });
+            $("#nav-about").addClass("active");
+        }
+        
+        if( scroll >= (portfolio_start * 0.90) && ( scroll <= (portfolio_end * 0.77) ) ){
+            $(".menu-icons-div").each( function(i){
+                $(this).removeClass("active");
+            });
+            $("#nav-portfolio").addClass("active");
+        }
+        
+        if( scroll >= (contact_start * 0.80) && ( scroll <= (contact_end * 0.80) ) ){
+            $(".menu-icons-div").each( function(i){
+                $(this).removeClass("active");
+            });
+            $("#nav-contact").addClass("active");
+        }
+        
+        
+    });
+    
+});
