a068e2ddfcc317780ef22ecbb9a5750629aa9d25
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-18 23:35:47 -0300

    menu y banner y punto

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
     $(window).scroll( function(){
         var scroll = $(window).scrollTop();
         
-        var home_start = $("#home").position().top;
-        var home_end = $("#home").position().top + $("#home").outerHeight();
+        //Obtengo el top y el bottom de cada sección
+        
+        var banner_start = $("#banner").position().top;
+        var banner_end = $("#banner").position().top + $("#banner").outerHeight();
         
         var about_start = $("#about-me").position().top;
         var about_end = $("#about-me").position().top + $("#about-me").outerHeight();
@@ -17,32 +19,34 @@ $(document).ready(function(){
         var contact_start = $("#contact").position().top;
         var contact_end = $("#contact").position().top + $("#contact").outerHeight();
         
-        if( scroll >= (home_start * 0.90) && ( scroll <= (home_end * 0.25) ) ){  
-            $(".menu-icons-div").each( function(i){
+        //Cuando scrollee, si la ventana muestra un % de la sección, se cambia el activo del menu
+        //remueve la clase Active del antiguo menu activo y se la agrega al menu que este en la seccion correspondiente
+        
+        if( scroll >= (banner_start * 1) && ( scroll <= (banner_end * 0.25) ) ){  
+            $(".nav-a").each( function(i){
                 $(this).removeClass("active");
             });
-            $("#nav-home").addClass("active");
+            $("#nav-banner").addClass("active");
         }
         
         if( ( scroll >= (about_start * 0.80) ) && ( scroll <= (about_end * 0.60) ) ){  
-            $(".menu-icons-div").each( function(i){
+            $(".nav-a").each( function(i){
                 $(this).removeClass("active");
             });
             $("#nav-about").addClass("active");
         }
-        
         if( scroll >= (portfolio_start * 0.90) && ( scroll <= (portfolio_end * 0.77) ) ){
-            $(".menu-icons-div").each( function(i){
+            $(".nav-a").each( function(i){
                 $(this).removeClass("active");
             });
             $("#nav-portfolio").addClass("active");
         }
         
         if( scroll >= (contact_start * 0.90) && ( scroll <= (contact_end * 0.80) ) ){
-            $(".menu-icons-div").each( function(i){
+            $(".nav-a").each( function(i){
                 $(this).removeClass("active");
             });
-            $("#nav-contact").addClass("active");
+            $("#nav-contact").toggleClass("active");
         }
         
         
