da0138d601137591550abeb2c5e724f5747162ba
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-19 01:07:08 -0300

    Update sticky-menu.js

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-//    $("nav-about").removeClass(".active");
-//    $("#nav-about").toggleClass("active");
-    console.log("prueba");
-//    $("#nav-about").css("color", "white");
+    
     
 });
