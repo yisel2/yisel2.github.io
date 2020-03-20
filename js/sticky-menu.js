6b8fbe453e29542e990043d5bd816f5beec65301
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-20 04:30:43 -0300

    sticky menu

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-    
+    $(window).scroll( function(){
+        var scroll = $(window).scrollTop();
+        var menu_top = $("#menu").position().top;
+        var menu_bottom = $("#menu").position().top + $("#menu").outerHeight();
+        var about_top = $("#about-me").position().top;
+        var banner_bottom = $("#banner").position().top + $("#banner").outerHeight();
+        var hidemenu_top = $("#hidemenu").position().top;
+        //si el scroll top position llega al top del menu entonces poner fixed al menu
+        
+        console.log("scroll: " + scroll + " menu_top: " + menu_top + " about_top: " + about_top);
+        
+        if(scroll >= menu_top){
+            $("#hidemenu").addClass("block");
+            $("#menu").addClass("fixed");
+        }
+        
+        if(scroll <= about_top && menu_bottom <= about_top){
+            $("#hidemenu").removeClass("block");
+            $("#menu").removeClass("fixed");
+        }
+    });
     
 });
