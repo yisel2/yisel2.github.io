d3ff92d3f2cb7c30a143d663b407b31b903ae976
Gisella Alaniz <61876806+yisel2@users.noreply.github.com>
2020-03-23 12:16:15 -0300

    retoques

--- js/sticky-menu.js
+++ js/sticky-menu.js
@@ -3,15 +3,12 @@ $(document).ready(function(){
         
         $(window).scroll(function(){
             var menu_top = $("#menu").position().top + 1;
-            console.log("scroll "+$(window).scrollTop()+" menutop "+menu_top+" bannerbot "+banner_bottom);
             if($(window).scrollTop() > menu_top){
                 $("#menu").addClass("fixed");
                 $("#hidemenu").addClass("block");
-                console.log("entro al primer if");
             }else if(menu_top < banner_bottom){
                 $("#hidemenu").removeClass("block");
                 $("#menu").removeClass("fixed");
-                console.log("entro al segundo if");
             }
         });            
 });
